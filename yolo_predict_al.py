85f1062972ab63f227b0e9aec45238973da3e645
kartik-jalal <kartikjalal@gmail.com>
2025-01-31 00:08:27 +0100

    python script for yolo predictions

---
+++ yolo_predict_al.py
@@ -0,0 +1,17 @@
+from ultralytics import YOLO
+import os
+
+# Load a model
+model = YOLO("/home/jalal/projects/data/neurocig/yolo/yolo11x-pose/weights/best.pt")  # pretrained YOLO11n model
+
+predict_dir = "/home/jalal/projects/data/neurocig/yolo/predict"
+
+predict_images = []
+
+for p_image in os.listdir(predict_dir):
+    if p_image.endswith('.jpg'):
+        p_image_path = os.path.join(predict_dir, p_image)
+        predict_images.append(p_image_path)
+
+# Run batched inference on a list of images
+model.predict(predict_images, save=True, save_txt=True, project=predict_dir, name='results')  # return a generator of Results objects
