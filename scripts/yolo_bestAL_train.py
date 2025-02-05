e65266e4174f701bbda352609da3aa79588dd4ac
kartik-jalal <kartikjalal@gmail.com>
2025-02-05 16:58:05 +0100

    checked the resume paramter to False and also added few comments on epoch and batch size based on the number of frames.

--- scripts/yolo_bestAL_train.py
+++ scripts/yolo_bestAL_train.py
@@ -4,4 +4,8 @@ from ultralytics import YOLO
 model = YOLO("/home/jalal/projects/data/neurocig/yolo/yolo11x-pose/weights/best.pt")  # load a pretrained model (recommended for training)
 
 # Train the model with 2 GPUs
-results = model.train(data="yolo_dataset.yaml", epochs=50, imgsz=640, batch=16, save=True, resume=True, project='/home/jalal/projects/data/neurocig/yolo')
+# 100 - 500 frames -> epoch 50 batch 16
+# 500 - 1000 frames - > epoch 70 batch 32
+# 1000 - 5000 frames -> epoch 100 batch 64
+# 5000 < frames -> epoch 200 / 300 batch 128
+results = model.train(data="yolo_dataset.yaml", epochs=50, imgsz=640, batch=16, save=True, resume=False, project='/home/jalal/projects/data/neurocig/yolo')
