eeeecf061d5fbf67f75531dcabd08f27285bcb17
kartik-jalal <kartikjalal@gmail.com>
2025-02-06 17:44:23 +0100

    Update the cycle number in the script

--- scripts/yolo_bestAL_train.py
+++ scripts/yolo_bestAL_train.py
@@ -9,7 +9,7 @@ model = YOLO(model_path)  # load a pretrained model (recommended for training)
 # 500 - 1000 frames - > epoch 70 batch 32
 # 1000 - 5000 frames -> epoch 100 batch 64
 # 5000 < frames -> epoch 200 / 300 batch 128
-cycle = 1
+cycle = 2
 name = f"cycle_{cycle}"
 
 results = model.train(data="yolo_dataset.yaml", epochs=50, imgsz=640, batch=16, save=True, resume=False, name=name, project='/home/jalal/projects/data/neurocig/yolo')
