3a032a72731e7d24d9fafd3cc80931e61faa7557
kartik-jalal <kartikjalal@gmail.com>
2025-02-12 11:45:20 +0100

    Added the yolo predict video script.

---
+++ scripts/yolo_predict_video.py
@@ -0,0 +1,19 @@
+from ultralytics import YOLO
+import os
+
+cycle = 5
+model_path = f"/home/jalal/projects/data/neurocig/yolo/cycle_{cycle}/weights/best.pt"
+# Load a model
+model = YOLO(model_path)  # pretrained YOLO11n model
+
+predict_dir = "/home/jalal/projects/data/neurocig/vids/processed/"
+
+# predict_images = []
+
+# for p_image in os.listdir(predict_dir):
+#     if p_image.endswith('.jpg'):
+#         p_image_path = os.path.join(predict_dir, p_image)
+#         predict_images.append(p_image_path)
+
+# Run batched inference on a list of images
+model.predict(os.path.join(predict_dir, "Gabbia2-D6-eCig(1)-pre.mp4"), stream_buffer=True ,save=True, save_txt=True, max_det=5, project=predict_dir, name='results')  # return a generator of Results objects
