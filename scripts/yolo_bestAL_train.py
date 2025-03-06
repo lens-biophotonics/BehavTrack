c1a6acfc5828a7d3e8a0b10e9e0779bd7437e626
kartik-jalal <kartikjalal@gmail.com>
2025-03-06 20:48:09 +0100

    updated the batch size to 24. Last 32 had memory erros.

--- scripts/yolo_bestAL_train.py
+++ scripts/yolo_bestAL_train.py
@@ -7,13 +7,13 @@ model = YOLO(model_path)  # load a pretrained model (recommended for training)
 
 # Train the model with GPUs
 # 100 - 500 frames -> epoch 50 batch 16
-# 500 - 1000 frames - > epoch 70 batch 32
+# 500 - 1000 frames - > epoch 70 batch 24
 # 1000 - 5000 frames -> epoch 100 batch 64
 # frames > 5000 -> epoch 200 / 300 batch 128
 new_cycle = prev_cycle + 1
 name = f"cycle_{new_cycle}"
 
 epochs = 70
-batch = 32
+batch = 24
 
 results = model.train(data="yolo_dataset.yaml", epochs=epochs, imgsz=640, batch=batch, save=True, resume=False, name=name, project='/home/jalal/projects/data/neurocig/yolo')
