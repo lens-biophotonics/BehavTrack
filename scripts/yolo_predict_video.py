38eaeb6871cd113bbbfca519e49a2d7d41e1bc95
kartik-jalal <kartikjalal@gmail.com>
2025-03-13 13:43:55 +0100

    updated the predict video script

--- scripts/yolo_predict_video.py
+++ scripts/yolo_predict_video.py
@@ -7,14 +7,22 @@ model_path = f"/home/jalal/projects/data/neurocig/yolo/cycle_{best_cycle}/weight
 model = YOLO(model_path)  # pretrained YOLO11n model
 
 vids_dir = "/home/jalal/projects/data/neurocig/vids/processed"
-output_dir = f"/home/jalal/projects/data/neurocig/vids/results/cycle_{best_cycle}/"
+output_dir = f"/home/jalal/projects/data/neurocig/vids/results/cycle_{best_cycle}/annotated"
 
-for vid_name in os.listdir(vids_dir):
-    if vid_name.endswith('.mp4'):
-        model.predict(os.path.join(vids_dir, vid_name),
-            stream_buffer=True,
-            save=True,
-            save_txt=True,
-            max_det=5,
-            project=vid_name
-        )
+# for vid_name in os.listdir(vids_dir):
+#     if vid_name.endswith('.mp4'):
+#         model.predict(os.path.join(vids_dir, vid_name),
+#             stream_buffer=True,
+#             save=True,
+#             save_txt=True,
+#             max_det=5,
+#             project=vid_name
+#         )
+
+model.predict(vids_dir,
+    stream_buffer=True,
+    save=True,
+    save_txt=True,
+    max_det=5,
+    project=output_dir
+)
