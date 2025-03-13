64a336b3b753d34a2d4fa6b5992839e2549e89c9
kartik-jalal <kartikjalal@gmail.com>
2025-03-13 13:33:37 +0100

    Fixed the yolo predict video script to make over all the videos.

--- scripts/yolo_predict_video.py
+++ scripts/yolo_predict_video.py
@@ -7,9 +7,14 @@ model_path = f"/home/jalal/projects/data/neurocig/yolo/cycle_{best_cycle}/weight
 model = YOLO(model_path)  # pretrained YOLO11n model
 
 vids_dir = "/home/jalal/projects/data/neurocig/vids/processed"
-output_dir = f"/home/jalal/projects/data/neurocig/vids/results/cycle_{best_cycle}/annotated_videos"
+output_dir = f"/home/jalal/projects/data/neurocig/vids/results/cycle_{best_cycle}/"
 
-output_folder = 'noTrack'
-
-# Run batched inference on a list of images
-model.predict(vids_dir, stream=True, stream_buffer=True ,save=True, save_txt=True, max_det=5, project=output_dir)  # return a generator of Results objects
+for vid_name in os.listdir(vids_dir):
+    if vid_name.endswith('.mp4'):
+        model.predict(os.path.join(vids_dir, vid_name),
+            streastream_buffer=True,
+            save=True,
+            save_txt=True,
+            max_det=5,
+            project=vid_name
+        )
