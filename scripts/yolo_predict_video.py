3fd4a6552c08c4e8218057a37c629375dd2c45c1
kartik-jalal <kartikjalal@gmail.com>
2025-03-13 13:26:43 +0100

    updated the video predict script

--- scripts/yolo_predict_video.py
+++ scripts/yolo_predict_video.py
@@ -1,22 +1,15 @@
 from ultralytics import YOLO
 import os
 
-cycle = 10
-model_path = f"/home/jalal/projects/data/neurocig/yolo/cycle_{cycle}/weights/best.pt"
+best_cycle = 9
+model_path = f"/home/jalal/projects/data/neurocig/yolo/cycle_{best_cycle}/weights/best.pt"
 # Load a model
 model = YOLO(model_path)  # pretrained YOLO11n model
 
-predict_dir = "/home/jalal/projects/data/neurocig/vids/processed"
-output_dir = f"/home/jalal/projects/data/neurocig/vids/results/cycle_{cycle}"
+vids_dir = "/home/jalal/projects/data/neurocig/vids/processed"
+output_dir = f"/home/jalal/projects/data/neurocig/vids/results/cycle_{best_cycle}/annotated_videos"
 
 output_folder = 'noTrack'
 
-# predict_images = []
-
-# for p_image in os.listdir(predict_dir):
-#     if p_image.endswith('.jpg'):
-#         p_image_path = os.path.join(predict_dir, p_image)
-#         predict_images.append(p_image_path)
-
 # Run batched inference on a list of images
-model.predict(os.path.join(predict_dir, "Gabbia2-D6-eCig(1)-pre.mp4"), stream_buffer=True ,save=True, save_txt=True, max_det=5, project=output_dir, name=output_folder)  # return a generator of Results objects
+model.predict(vids_dir, stream=True, stream_buffer=True ,save=True, save_txt=True, max_det=5, project=output_dir)  # return a generator of Results objects
