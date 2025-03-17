273e2c8baf95bff2467f0f1571256d1935d300dc
kartik-jalal <kartikjalal@gmail.com>
2025-03-17 16:09:54 +0100

    Added the option for tracking\

--- scripts/yolo_predict_video.py
+++ scripts/yolo_predict_video.py
@@ -10,9 +10,16 @@ model = YOLO(model_path)  # pretrained YOLO11n model
 vids_dir = "/home/jalal/projects/data/neurocig/vids/processed"
 output_dir = f"/home/jalal/projects/data/neurocig/vids/results/cycle_{best_cycle}/annotated"
 
+track = True
+if track:
+    output_dir = output_dir + '_tracked'
+
+
 for vid_name in tqdm(os.listdir(vids_dir)):
     if vid_name.endswith('.mp4'):
-        model.predict(os.path.join(vids_dir, vid_name),
+        if track:
+            # Tracking with BotSort
+            model.track(os.path.join(vids_dir, vid_name),
             stream_buffer=True,
             save=True,
             save_txt=True,
@@ -20,3 +27,12 @@ for vid_name in tqdm(os.listdir(vids_dir)):
             project=output_dir,
             name=vid_name.removesuffix('.mp4')
         )
+        else:
+            model.predict(os.path.join(vids_dir, vid_name),
+                stream_buffer=True,
+                save=True,
+                save_txt=True,
+                max_det=5,
+                project=output_dir,
+                name=vid_name.removesuffix('.mp4')
+            )
