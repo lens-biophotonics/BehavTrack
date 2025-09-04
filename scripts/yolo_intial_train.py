2efdb55d27a1f1977c93a3833671134d835c07c6
kartik-jalal <kartikjalal@gmail.com>
2025-09-04 16:03:21 +0200

    removed the old scripts

--- scripts/yolo_intial_train.py
+++
@@ -1,7 +0,0 @@
-from ultralytics import YOLO
-
-# Load a model
-model = YOLO("yolo11x-pose.pt")  # load a pretrained model (recommended for training)
-
-# Train the model with 2 GPUs
-results = model.train(data="yolo_dataset.yaml", epochs=50, imgsz=640, batch=16, save=True, name="Initial_run", project='/home/jalal/projects/data/neurocig/yolo')
